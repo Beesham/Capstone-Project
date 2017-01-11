9a54255afc8aad478f49fb273d9c022ba0efdcf1
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-01-10 21:24:12 -0500

    Build Detail layout

--- app/src/main/java/com/beesham/beerac/ui/HomeActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/HomeActivity.java
@@ -1,11 +1,16 @@
-package com.beesham.beerac;
+package com.beesham.beerac.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.beesham.beerac.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +20,7 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 public class HomeActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.photo) ImageView mBeerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,13 @@ public class HomeActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
+        mBeerImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, DetailsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
