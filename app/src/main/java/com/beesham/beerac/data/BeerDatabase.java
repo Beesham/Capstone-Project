61cb9ed1a1a22e6ad33dded4ea34c9fe2d1fb1cc
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-01-17 20:47:12 -0500

    contentProvider: add databse and provider

--- app/src/main/java/com/beesham/beerac/data/BeerDatabase.java
+++ app/src/main/java/com/beesham/beerac/data/BeerDatabase.java
@@ -1,8 +1,15 @@
 package com.beesham.beerac.data;
 
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
 /**
  * Created by Beesham on 1/17/2017.
  */
 
+@Database(version = BeerDatabase.VERSION)
 public class BeerDatabase {
+    public static final int VERSION = 1;
+
+    @Table(BeerColumns.class) public static final String BEERS = "beers";
 }
