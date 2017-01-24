d1572ce8521f9902b743e4e3bdfff124465c6b60
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-01-23 20:31:11 -0500

    fix: search view launch search activity

--- app/src/main/java/com/beesham/beerac/ui/SearchActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/SearchActivity.java
@@ -1,15 +1,29 @@
 package com.beesham.beerac.ui;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.beesham.beerac.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SearchActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Log.v(LOG_TAG, "inSearchAct");
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.v(LOG_TAG, "query: " + query);
+        }
     }
 }
