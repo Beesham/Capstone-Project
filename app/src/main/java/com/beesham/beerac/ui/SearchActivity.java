9aaeec6440b6d51b9dd9484ba3844864322c8efe
beesham <sarendranauth.beesham@gmail.com>
2017-01-25 19:42:35 -0500

    add new table

--- app/src/main/java/com/beesham/beerac/ui/SearchActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/SearchActivity.java
@@ -4,19 +4,33 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.beesham.beerac.R;
 import com.beesham.beerac.service.BeerACIntentService;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SearchActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = SearchActivity.class.getSimpleName();
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.beers_recycler_view) RecyclerView mRecyclerView;
+
+    private BeerRecyclerViewAdapter mBeerRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
@@ -26,6 +40,9 @@ public class SearchActivity extends AppCompatActivity {
 
             launchBeerACIntentService(query);
         }
+
+        //mBeerRecyclerViewAdapter = new BeerRecyclerViewAdapter();
+        mRecyclerView.setAdapter(mBeerRecyclerViewAdapter);
     }
 
     private void launchBeerACIntentService(String queryString){
