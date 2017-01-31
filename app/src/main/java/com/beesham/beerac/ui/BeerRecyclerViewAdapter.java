eb7a719d1212637c72c297e51dd47344429d5ca2
beesham <sarendranauth.beesham@gmail.com>
2017-01-31 13:28:17 -0500

    display data

--- app/src/main/java/com/beesham/beerac/ui/BeerRecyclerViewAdapter.java
+++ app/src/main/java/com/beesham/beerac/ui/BeerRecyclerViewAdapter.java
@@ -1,8 +1,11 @@
 package com.beesham.beerac.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,51 +14,99 @@ import android.widget.TextView;
 
 import com.beesham.beerac.R;
 import com.beesham.beerac.data.Columns;
+import com.beesham.beerac.service.BeerACIntentService;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.beesham.beerac.service.BeerACIntentService.ACTION_GET_BEERS;
+import static com.beesham.beerac.service.BeerACIntentService.ACTION_GET_BEER_DETAILS;
 
 /**
  * Created by beesham on 24/01/17.
  */
 
-public class BeerRecyclerViewAdapter extends RecyclerView.Adapter<BeerRecyclerViewAdapter.ViewHolder> {
+public class BeerRecyclerViewAdapter extends RecyclerView.Adapter<BeerRecyclerViewAdapter.BeerViewHolder> {
 
-    private ArrayList<Beer> mBeerList;
+    private static final String LOG_TAG = BeerRecyclerViewAdapter.class.getSimpleName();
+
     private final Context mContext;
+    private final BeerRecyclerViewAdapterOnClickHandler mOnClickHandler;
+
     private Cursor mCursor;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView beer_icon_imageView;
-        public ImageView drinkBeer_icon_imageView;
-        public TextView beer_name_textView;
-        public ViewHolder(View v) {
+    public static interface BeerRecyclerViewAdapterOnClickHandler{
+        void onClick(BeerViewHolder beerViewHolder);
+    }
+
+    public class BeerViewHolder extends RecyclerView.ViewHolder
+                                    implements View.OnClickListener{
+        @BindView(R.id.beer_image_image_view) ImageView beer_icon_imageView;
+        @BindView(R.id.drink_beer_image_view) ImageView drinkBeer_icon_imageView;
+        @BindView(R.id.beer_name_text_view) TextView beer_name_textView;
+
+        public BeerViewHolder(View v) {
             super(v);
-            beer_icon_imageView = (ImageView) v.findViewById(R.id.beer_image_image_view);
-            drinkBeer_icon_imageView = (ImageView) v.findViewById(R.id.drink_beer_image_view);
-            beer_name_textView = (TextView) v.findViewById(R.id.beer_name_text_view);
+            ButterKnife.bind(this, v);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.v(LOG_TAG, "I was clicked " + beer_name_textView.getText());
+            mOnClickHandler.onClick(this);
+
+            mCursor.moveToPosition(getPosition());
+
+            Bundle args = new Bundle();
+            args.putString("uri", BeerACIntentService.buildBeerByIdUri(
+                    mCursor.getString(mCursor.getColumnIndex(Columns.SearchedBeerColumns.BEERID))
+            ));
+
+
+
+            mContext.startActivity(new Intent(mContext, DetailsActivity.class)
+                    .putExtras(args));
         }
     }
 
-    public BeerRecyclerViewAdapter(Context context) {
+    public BeerRecyclerViewAdapter(Context context,
+                                   BeerRecyclerViewAdapterOnClickHandler handler) {
         mContext = context;
+        mOnClickHandler = handler;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public BeerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.beer_list_item, parent, false);
 
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        BeerViewHolder beerViewHolder = new BeerViewHolder(view);
+        return beerViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(BeerViewHolder holder, int position) {
         mCursor.moveToPosition(position);
 
-        //holder.drinkBeer_icon_imageView.setImageResource(beer.getUrl_icon());
+        if(mCursor.getString(
+                mCursor.getColumnIndex(Columns.SearchedBeerColumns.LABELS))
+                .equals("Y")) {
+
+            String imagePath = mCursor.getString(
+                    mCursor.getColumnIndex(
+                            Columns.SearchedBeerColumns.IMAGEURLMEDIUM)
+            );
+
+            Picasso.with(mContext)
+                    .load(imagePath)
+                    .resize(150, 150)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(holder.beer_icon_imageView);
+        }
         holder.beer_name_textView.setText(mCursor.getString(
                 mCursor.getColumnIndex(Columns.SearchedBeerColumns.NAME)));
         holder.drinkBeer_icon_imageView.setImageResource(R.mipmap.ic_launcher);
--- app/src/main/java/com/beesham/beerac/ui/Utils.java
+++ app/src/main/java/com/beesham/beerac/ui/Utils.java
@@ -1,18 +1,45 @@
 package com.beesham.beerac.ui;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
+
+import com.beesham.beerac.data.Columns;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Vector;
 
 /**
  * Created by beesham on 21/01/17.
  */
 
 public class Utils {
+
+    static final String KEY_CURRENTPAGE = "currentPage";
+    static final String KEY_NUMBEROFPAGES = "numberOfPages";
+    static final String KEY_TOTALRESULTS = "totalResults";
+    static final String KEY_DATA = "data";
+
+    static final String KEY_NAME = "name";
+    static final String KEY_BEERID = "id";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_FOODPARINGS = "foodParings";
+    static final String KEY_ABV = "abv";
+    static final String KEY_ISORGANIC = "isOrganic";
+    static final String KEY_LABELS = "labels";
+    static final String KEY_IMAGEURL_ICON = "icon";
+    static final String KEY_IMAGEURL_MEDIUM = "medium";
+    static final String KEY_IMAGEURL_LARGE = "large";
+    static final String KEY_YEAR = "year";
+
 
     public static String getCountryName(Context context, double latitude, double longitude) throws IOException {
         Geocoder geocoder = new Geocoder(context, Locale.getDefault());
@@ -22,5 +49,107 @@ public class Utils {
             return addresses.get(0).getCountryName();
         }
         return null;
+    }
+
+    public static Vector<ContentValues> extractBeers(String jsonResponse) throws JSONException {
+
+        int currentPage;
+        int numberOfPages;
+        int totalResults;
+
+        JSONObject beerListJsonObj = new JSONObject(jsonResponse);
+
+        currentPage = beerListJsonObj.getInt(KEY_CURRENTPAGE);
+        numberOfPages = beerListJsonObj.getInt(KEY_NUMBEROFPAGES);
+        totalResults = beerListJsonObj.getInt(KEY_TOTALRESULTS);
+
+        JSONArray beerListJsonArray = beerListJsonObj.getJSONArray(KEY_DATA);
+
+        Vector<ContentValues> contentValuesVector = new Vector<>(beerListJsonArray.length());
+
+        for(int i = 0; i < beerListJsonArray.length(); i++){
+            String name;
+            String id;
+            String description = null;
+            String labels;
+            String imageUrlIcon = null;
+            String imageUrlMedium = null;
+            String imageUrlLarge = null;
+
+            name =  beerListJsonArray.getJSONObject(i).getString(KEY_NAME);
+            id =  beerListJsonArray.getJSONObject(i).getString(KEY_BEERID);
+
+
+            if(beerListJsonArray.getJSONObject(i).has(KEY_DESCRIPTION)) {
+                description = beerListJsonArray.getJSONObject(i)
+                        .getString(KEY_DESCRIPTION);
+            }
+
+            if(beerListJsonArray.getJSONObject(i).has(KEY_LABELS)) {
+                labels = "Y";
+
+                imageUrlIcon = beerListJsonArray.getJSONObject(i)
+                        .getJSONObject(KEY_LABELS)
+                        .getString(KEY_IMAGEURL_ICON);
+
+                imageUrlMedium = beerListJsonArray.getJSONObject(i)
+                        .getJSONObject(KEY_LABELS)
+                        .getString(KEY_IMAGEURL_MEDIUM);
+
+                imageUrlLarge = beerListJsonArray.getJSONObject(i)
+                        .getJSONObject(KEY_LABELS)
+                        .getString(KEY_IMAGEURL_LARGE);
+            }else{
+                labels = "N";
+            }
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(Columns.SearchedBeerColumns.NAME, name);
+            contentValues.put(Columns.SearchedBeerColumns.BEERID, id);
+            contentValues.put(Columns.SearchedBeerColumns.DESCRIPTION, description);
+            contentValues.put(Columns.SearchedBeerColumns.LABELS, labels);
+
+            if(labels.equals("Y")){
+                contentValues.put(Columns.SearchedBeerColumns.IMAGEURLICON, imageUrlIcon);
+                contentValues.put(Columns.SearchedBeerColumns.IMAGEURLLARGE, imageUrlMedium);
+                contentValues.put(Columns.SearchedBeerColumns.IMAGEURLMEDIUM, imageUrlLarge);
+            }
+
+            contentValuesVector.add(contentValues);
+        }
+
+        return contentValuesVector;
+    }
+
+    public static Beer extractBeerDetails(String jsonResponse) throws JSONException {
+        JSONObject beerJsonObj = new JSONObject(jsonResponse);
+
+        String name;
+        String id;
+        String description = "";
+        String imageUrlLarge = null;
+
+        boolean hasImages = false;
+
+        if(!beerJsonObj.getString("status").equals("success")) return null;
+
+        JSONObject dataJsonObj = beerJsonObj.getJSONObject(KEY_DATA);
+
+        name = dataJsonObj.getString(KEY_NAME);
+
+        if(dataJsonObj.has(KEY_DESCRIPTION)) {
+            description = dataJsonObj.getString(KEY_DESCRIPTION);
+        }
+
+        if(dataJsonObj.has(KEY_LABELS)){
+            hasImages = true;
+            JSONObject imagesJsonObj = dataJsonObj.getJSONObject(KEY_LABELS);
+            imageUrlLarge = imagesJsonObj.getString(KEY_IMAGEURL_LARGE);
+        }
+
+        Beer beer = new Beer(name, null, description, null, hasImages, 0);
+        if(hasImages) beer.setUrl_large(imageUrlLarge);
+
+        return beer;
     }
 }
