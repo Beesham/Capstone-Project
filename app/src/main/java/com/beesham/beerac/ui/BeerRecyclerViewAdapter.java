6492eb0e0fcafa32a95ff922da73196be377a6ae
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-02-02 19:15:45 -0500

    display beer in Home

--- app/src/main/java/com/beesham/beerac/ui/BeerRecyclerViewAdapter.java
+++ app/src/main/java/com/beesham/beerac/ui/BeerRecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.beesham.beerac.ui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -77,11 +79,34 @@ public class BeerRecyclerViewAdapter extends RecyclerView.Adapter<BeerRecyclerVi
                 public void onClick(View view) {
                     mCursor.moveToPosition(getPosition());
 
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-                    prefs.edit()
-                            .putString("drink_beer", mCursor.getString(mCursor.getColumnIndex(Columns.SearchedBeerColumns.BEERID)))
-                    .commit();
-                    Log.v(LOG_TAG, "I want to drink this beer");
+                    final SharedPreferences prefs = mContext.getSharedPreferences("com.drink_beer", Context.MODE_PRIVATE); //PreferenceManager.getDefaultSharedPreferences(mContext);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setTitle("Drink this beer?");
+                    builder.setMessage("This beer will be saved under \"My beers\"");
+                    builder.setPositiveButton("0k", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            String beerId =  mCursor.getString(mCursor.getColumnIndex(Columns.SearchedBeerColumns.BEERID));
+                            prefs.edit()
+                                    .putString("drink_beer", beerId)
+                                    .commit();
+
+                            Intent intent = new Intent(mContext, BeerACIntentService.class);
+                            intent.setAction(ACTION_GET_BEER_DETAILS);
+                            intent.putExtra(BeerACIntentService.EXTRA_QUERY, beerId);
+                            BeerACIntentService.startBeerQueryService(mContext, intent);
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                    Log.v(LOG_TAG, "I want to drink this beer: " + mCursor.getString(mCursor.getColumnIndex(Columns.SearchedBeerColumns.BEERID)));
                 }
             });
         }
