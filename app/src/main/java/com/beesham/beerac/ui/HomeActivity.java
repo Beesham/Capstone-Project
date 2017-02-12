09cf238a2fe740098558b223243e0f5db5f2a600
beesham <sarendranauth.beesham@gmail.com>
2017-02-12 11:45:38 -0500

    fix: home and detail init

--- app/src/main/java/com/beesham/beerac/ui/HomeActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/HomeActivity.java
@@ -1,20 +1,33 @@
 package com.beesham.beerac.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.beesham.beerac.R;
+import com.beesham.beerac.service.BeerACIntentService;
+
+import static com.beesham.beerac.service.BeerACIntentService.ACTION_GET_BEER_DETAILS;
+import static com.beesham.beerac.service.BeerACIntentService.buildBeerByIdUri;
 
 public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener{ //implements LoaderManager.LoaderCallbacks<Cursor>,        TimePickerFragment.TimeSetter{
 
     private static final String LOG_TAG = HomeActivity.class.getSimpleName();
+    public static int FIRST_LAUNCH_CODE = 0;
+    public static int CONCEQUENT_LAUNCH_CODE = 1;
 
+    private int launchCode;
 
     private boolean mTwoPane;
+    private String mUri;
 
-    private final String DETAIL_ACTIVITY_FRAG_TAG = "detail_frag";
+
+    private final String DETAIL_ACTIVITY_FRAG_TAG = "DETAIL_FRAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +36,45 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFr
 
         if (findViewById(R.id.beer_detail_container) != null) {
             mTwoPane = true;
+            Log.v(LOG_TAG, "twoPane");
+
+            DetailsActivityFragment fragment = new DetailsActivityFragment();
+            Bundle args = new Bundle();
+
+            Log.v(LOG_TAG, "uri: " + buildBeerByIdUri(Utils.getBeerIdFromPrefs(this)));
+
+
+            args.putString(getString(R.string.beer_details_uri_key),
+                    buildBeerByIdUri(Utils.getBeerIdFromPrefs(this)));
+            args.putInt(getString(R.string.launch_key), launchCode);
+            fragment.setArguments(args);
+
             if (savedInstanceState == null) {
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.beer_detail_container, new DetailsActivityFragment(), DETAIL_ACTIVITY_FRAG_TAG)
+                        .replace(R.id.beer_detail_container, fragment, DETAIL_ACTIVITY_FRAG_TAG)
                         .commit();
             }
         } else {
             mTwoPane = false;
         }
 
+
     }
 
 
     @Override
-    public void onFragmentInteraction(Uri uri) {
-        //TODO
+    public void onFragmentInteraction(String uri) {
+        //mUri = uri;
+        /*if(mTwoPane){
+            DetailsActivityFragment fragment = new DetailsActivityFragment();
+            Bundle args = new Bundle();
+            args.putString(getString(R.string.beer_details_uri_key), uri);
+            fragment.setArguments(args);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.beer_detail_container, fragment, DETAIL_ACTIVITY_FRAG_TAG)
+                    .commit();
+        }*/
     }
 
 
