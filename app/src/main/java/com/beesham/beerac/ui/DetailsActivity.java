0027b37147ecedd06262a050d34850f7b7bc04b3
beesham <sarendranauth.beesham@gmail.com>
2017-02-12 14:36:01 -0500

    fix: actionBar and beer details onClick

--- app/src/main/java/com/beesham/beerac/ui/DetailsActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/DetailsActivity.java
@@ -1,10 +1,12 @@
 package com.beesham.beerac.ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.beesham.beerac.R;
@@ -17,13 +19,21 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle bundle = getIntent().getExtras();
+        /*if(bundle != null) {
+            if (bundle.containsKey(getString(R.string.beer_details_uri_key))) {
+                mUri = Uri.parse(bundle.getString(getString(R.string.beer_details_uri_key)));
+                Log.v(LOG_TAG, mUri.toString());
+            }
+        }
+*/
+        DetailsActivityFragment fragment = new DetailsActivityFragment();
+        fragment.setArguments(bundle);
 
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.beer_detail_container, new DetailsActivityFragment())
+                    .add(R.id.beer_detail_container, fragment)
                     .commit();
         }
 
