11c39b81edd931ffabff84ec2b56f4c9459f99d2
beesham <sarendranauth.beesham@gmail.com>
2017-02-14 12:35:57 -0500

    feat: item selection

--- app/src/main/java/com/beesham/beerac/ui/SavesActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/SavesActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.ListView;
 
 import com.beesham.beerac.R;
 import com.beesham.beerac.analytics.AnalyticsApplication;
@@ -35,6 +37,10 @@ public class SavesActivity extends AppCompatActivity  implements LoaderManager.L
     private RecyclerView mRecyclerView;
     private Cursor mCursor;
 
+    private int mPosition = RecyclerView.NO_POSITION;
+    private int mChoiceMode;
+    private boolean mAutoSelectView;
+
     private Tracker mTracker;
 
     @Override
@@ -48,12 +54,12 @@ public class SavesActivity extends AppCompatActivity  implements LoaderManager.L
 
         mBeerRecyclerViewAdapter = new BeerRecyclerViewAdapter(this, new BeerRecyclerViewAdapter.BeerRecyclerViewAdapterOnClickHandler() {
             @Override
-            public void onClick(Bundle bundle) {
+            public void onClick(Bundle bundle, BeerRecyclerViewAdapter.BeerViewHolder beerViewHolder) {
 
                 Log.v(LOG_TAG, "I was clicked: ");
 
             }
-        });
+        }, mChoiceMode);
         mRecyclerView = (RecyclerView) findViewById(R.id.beer_recycler_view);
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -102,6 +108,29 @@ public class SavesActivity extends AppCompatActivity  implements LoaderManager.L
         if(!mCursor.moveToFirst()) {return;}
 
         mBeerRecyclerViewAdapter.swapCursor(mCursor);
+        if(mPosition != ListView.INVALID_POSITION)  mRecyclerView.smoothScrollToPosition(mPosition);
+
+        if(data.getCount() > 0){
+            mRecyclerView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    // Since we know we're going to get items, we keep the listener around until
+                    // we see Children.
+                    if (mRecyclerView.getChildCount() > 0) {
+                        mRecyclerView.getViewTreeObserver().removeOnPreDrawListener(this);
+                        int itemPosition = mBeerRecyclerViewAdapter.getSelectedItemPosition();
+                        if ( RecyclerView.NO_POSITION == itemPosition ) itemPosition = 0;
+                        RecyclerView.ViewHolder vh = mRecyclerView.findViewHolderForAdapterPosition(itemPosition);
+                        if ( null != vh && mAutoSelectView ) {
+                            mBeerRecyclerViewAdapter.selectView( vh );
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+        }
 
         Log.v(LOG_TAG, "size of cursor: " + mCursor.getCount());
     }
