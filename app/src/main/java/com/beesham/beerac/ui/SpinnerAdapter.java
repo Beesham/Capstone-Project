78869c6efc0e474da5f5307b18a123cd40a9273a
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-02-24 20:21:33 -0500

    refactor: convert volume edittext to spinner

---
+++ app/src/main/java/com/beesham/beerac/ui/SpinnerAdapter.java
@@ -0,0 +1,75 @@
+package com.beesham.beerac.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.beesham.beerac.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+/**
+ * Created by beesham on 23/02/17.
+ */
+
+public class SpinnerAdapter extends BaseAdapter {
+
+    Context mContext;
+    ArrayList<String> mVolumeArray;
+    String[] mUnitsArray;
+
+    @BindView(R.id.volume_text_view) TextView mVolumerTextView;
+    @BindView(R.id.units_text_view) TextView mUnitsTextView;
+
+    public SpinnerAdapter(Context context, String[] volumeArray, String[] unitsArray) {
+        mContext = context;
+        mVolumeArray = new ArrayList<String>();
+        mVolumeArray.addAll(Arrays.asList(volumeArray));
+        mUnitsArray = unitsArray;
+    }
+
+    @Override
+    public int getCount() {
+        return mVolumeArray.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mVolumeArray.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        view = View.inflate(mContext, R.layout.volume_spinner_item, null);
+        ButterKnife.bind(this, view);
+        mVolumerTextView.setText((String) mVolumeArray.get(position));
+        mUnitsTextView.setText(mUnitsArray[0]);
+        return view;
+    }
+
+    /**
+     * Returns the position of the specified item in the array.
+     *
+     * @param item The item to retrieve the position of.
+     *
+     * @return The position of the specified item.
+     */
+    public int getPosition(@Nullable String item) {
+        return mVolumeArray.indexOf(item);
+    }
+
+}
