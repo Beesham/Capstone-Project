d229691ba0aa07d9e9dc64394ec669b9540c2138
beesham <sarendranauth.beesham@gmail.com>
2017-02-28 14:26:30 -0500

    refactor: config installRelease task | fix: time reset upon activity start

---
+++ app/src/test/java/com/beesham/beerac/ui/UtilsTestSuite.java
@@ -0,0 +1,18 @@
+package com.beesham.beerac.ui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by beesham on 28/02/17.
+ */
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        UtilsTest.class
+})
+
+
+public class UtilsTestSuite {
+}
