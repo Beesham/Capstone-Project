7c14f92f803596728f35044a05c38ccfb9a47919
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-03-12 18:57:03 -0400

    refactor: move collapsing toolbar to activity in Details

--- app/src/main/java/com/beesham/beerac/ui/DetailsActivity.java
+++ app/src/main/java/com/beesham/beerac/ui/DetailsActivity.java
@@ -1,18 +1,35 @@
 package com.beesham.beerac.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.beesham.beerac.R;
+import com.squareup.picasso.Picasso;
 
-public class DetailsActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static java.lang.System.load;
+
+public class DetailsActivity extends AppCompatActivity implements DetailsActivityFragment.OnFragmentInteractionListener{
 
     private static final String LOG_TAG = DetailsActivity.class.getSimpleName();
+
+    @BindView(R.id.title_scrim_view) View mTitleScrimView;
+    @BindView(R.id.photo) ImageView beerImageView;
+    @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout mCollapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -25,6 +42,24 @@ public class DetailsActivity extends AppCompatActivity {
                     .commit();
         }
 
+        if(!HomeActivity.mTwoPane) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_navigate_home));
+        }
     }
 
+    @Override
+    public void onFragmentInteraction(Beer beer) {
+        mCollapsingToolbar.setTitle(beer.getName());
+
+        if (!TextUtils.isEmpty(beer.getUrl_large())) {
+            Picasso.with(this)
+                    .load(beer.getUrl_large())
+                    .placeholder(R.drawable.stockbeer)
+                    .error(R.drawable.stockbeer)
+                    .into(beerImageView);
+        }
+    }
 }
