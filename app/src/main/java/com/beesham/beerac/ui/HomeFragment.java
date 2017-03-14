dd1247df0fdadc8d279f5e234639cfb4b1f15629
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-03-14 19:13:24 -0400

    refactor: rever back to solid color ab

--- app/src/main/java/com/beesham/beerac/ui/HomeFragment.java
+++ app/src/main/java/com/beesham/beerac/ui/HomeFragment.java
@@ -17,6 +17,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.Menu;
