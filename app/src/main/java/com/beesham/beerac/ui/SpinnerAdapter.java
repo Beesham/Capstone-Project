d4f366883c2e5ee472362f1f326ccbe5da5ad10b
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-03-21 16:05:26 -0400

    Fix: wrong units in Home

--- app/src/main/java/com/beesham/beerac/ui/SpinnerAdapter.java
+++ app/src/main/java/com/beesham/beerac/ui/SpinnerAdapter.java
@@ -61,12 +61,12 @@ public class SpinnerAdapter extends BaseAdapter {
 
         if(preferences.getString(mContext.getString(R.string.pref_units_key),
                 mContext.getString(R.string.pref_units_default)).equals(mContext.getString(R.string.pref_units_default))) {
+            mUnitsTextView.setText(mUnitsArray[0]);
+            mVolumeTextView.setText((String) Double.toString(Utils.mLToOz(Integer.parseInt(mVolumeArray.get(position)))));
+        }else{
             mUnitsTextView.setText(mUnitsArray[1]);
             mVolumeTextView.setText((String) mVolumeArray.get(position));
 
-        }else{
-            mUnitsTextView.setText(mUnitsArray[0]);
-            mVolumeTextView.setText((String) Double.toString(Utils.mLToOz(Integer.parseInt(mVolumeArray.get(position)))));
         }
         return view;
     }
